284986bc34e4f517b3df5d02bd74a944162e6059
zgfzgf <1901989065@qq.com>
2021-06-04 18:12:05 +0800

    add mining

---
+++ interfaces/sushi/IMigratorChef.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.12;
+
+interface IMigratorChef {
+    // Perform LP token migration from legacy UniswapV2 to SushiSwap.
+    // Take the current LP token address and return the new LP token address.
+    // Migrator should have full access to the caller's LP token.
+    // Return the new LP token address.
+    //
+    // XXX Migrator must have allowance access to UniswapV2 LP tokens.
+    // SushiSwap must mint EXACTLY the same amount of SushiSwap LP tokens or
+    // else something bad will happen. Traditional UniswapV2 does not
+    // do that so be careful!
+    function migrate(address token) external returns (address);
+}
